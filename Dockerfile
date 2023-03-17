676b7bf04be578a2f26a9eed103ce0f87d5bc8e8
haandol <ldg55d@gmail.com>
2023-03-17 14:34:19 +0900

    update train.py to train

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM  --platform=linux/amd64 763104351884.dkr.ecr.ap-northeast-2.amazonaws.com/pytorch-training:1.13.1-cpu-py39
+FROM  --platform=linux/amd64 763104351884.dkr.ecr.ap-northeast-2.amazonaws.com/pytorch-training:1.13.1-gpu-py39
 
 RUN pip3 install ultralytics
 
-COPY yolov8n.pt /opt/ml/code/yolov8n.pt
 COPY train.py /opt/ml/code/train.py
 
 ENV SAGEMAKER_PROGRAM train.py
--- train.py
+++ train.py
@@ -1,28 +1,61 @@
 import os
+import yaml
 import torch
 import argparse
+from pathlib import Path
 from ultralytics import YOLO
 
 
-if __name__=='__main__':
-
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # hyperparameters sent by the client are passed as command-line arguments to the script.
-    parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--learning-rate', type=float, default=0.05)
-    parser.add_argument('--use-cuda', type=bool, default=False)
+    # hyperparameters sent by the Estimator
+    parser.add_argument('--model', type=str, default=os.environ.get('SM_HP_MODEL', 'yolov8n'))
+    parser.add_argument('--experiment', type=str, default=os.environ['SM_HP_EXPERIMENT'])
+    parser.add_argument('--epochs', type=int, default=os.environ['SM_HP_EPOCHS'])
+    parser.add_argument('--batch', type=int, default=os.environ['SM_HP_BATCH'])
+    parser.add_argument('--imgsz', type=int, default=os.environ['SM_HP_IMGSZ'])
+    parser.add_argument('--seed', type=int, default=os.environ['SM_HP_SEED'])
 
     # Data, model, and output directories
-    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    parser.add_argument('--test', type=str, default=os.environ.get('SM_CHANNEL_TEST', None))
+    parser.add_argument('--valid', type=str, default=os.environ['SM_CHANNEL_VALID'])
 
     args, _ = parser.parse_known_args()
 
-    model = YOLO('yolov8n.pt')
+    # Set the device
+    device = ','.join(','.join(map(lambda x: str(x), range(1)))) if torch.cuda.is_available() else 'cpu'
 
-    with open(os.path.join(args.model_dir, 'model.pth'), 'wb') as f:
+    # Load the model
+    model = YOLO(f'{args.model}.pt', device=device)
+
+    # modify data conf
+    data_conf_path = os.path.join(args.train, 'data.yaml')
+    with open(data_conf_path, 'r') as fp:
+        data = yaml.load(fp.read())
+        data['train'] = str(Path(args.train).parent)
+        data['valid'] = str(Path(args.valid).parent)
+    
+    with open(data_conf_path, 'w') as fp:
+        fp.write(yaml.dump(data, fp))
+
+    # Training
+    model.train(
+        data=data_conf_path,
+        name=args.experiment,
+        epochs=args.epochs,
+        batch=args.batch,
+        imgsz=args.imgsz,
+        seed=args.seed,
+        device=device,
+        exist_ok=True,
+    )
+
+    # Save the model as PyTorch model
+    best_pt_path = os.path.join('runs', 'detect', args.experiment, 'weights', 'best.pt')
+    output_pt_path = os.path.join(args.model_dir, 'model.pth')
+
+    model = YOLO(best_pt_path)
+    with open(output_pt_path, 'wb') as f:
         torch.save(model.model.state_dict(), f)
